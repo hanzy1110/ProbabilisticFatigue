b4e85f28a9a812491c41e0178b03b24c15d2a938
hanzy1110 <hanzy1110@github.com>
2024-01-04 10:43:25 -0300

    many 2 many

--- copyFolders.sh
+++
@@ -1,2 +0,0 @@
-cp -r Results1 /mnt/c/Users/blueman69/Documents/KriginFatigue
-cp -r Results2 /mnt/c/Users/blueman69/Documents/KriginFatigue
--- restartSamples.sh
+++
@@ -1,2 +0,0 @@
-rm Results1/lifeSamples.npz
-rm Results2/loadSamples.npz
