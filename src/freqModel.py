3029e1a255e1367ef0ead41ad76dda50d192e47f
hanzy1110 <hanzy1110@github.com>
2024-01-23 15:04:30 -0300

    scaling

--- src/freqModel.py
+++ src/freqModel.py
@@ -53,17 +53,17 @@ def calculate_freq_dist(freq_data: pathlib.Path, plot=False):
 
 
 def total_cycles_per_year(
-    cycling_hours, n_years, freq_data, ndraws=1, ls=0.8, tau=10.0
+    cycling_hours, n_years, freq_data, ndraws=1, ls=1.0, tau=50.0
 ):
     freq = calculate_freq_dist(freq_data)
     print(f"frequency => {freq}")
     n_mean = cycling_hours * freq.mean() * 3600
     # return n_mean * np.ones_like(np.arange(n_years))
     # Use the follwing when modelling random amounts:
-    cov = n_mean * pm.gp.cov.Matern52(1, ls)
+    cov = (n_mean / tau) * pm.gp.cov.Matern52(1, ls)
     X = np.linspace(0, n_years, n_years)[:, None]
     K = cov(X).eval()
-    mu = np.ones(len(K), dtype=np.float64)
+    mu = n_mean * np.ones(len(K))
     cycles = pm.draw(
         pm.MvNormal.dist(mu=mu, cov=K, shape=len(K)), draws=ndraws, random_seed=rng
     ).T
