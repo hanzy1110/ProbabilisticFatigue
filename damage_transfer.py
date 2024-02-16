7322c81de85c441fc77b01dc7947036261a8a45b
hanzy1110 <hanzy1110@github.com>
2024-02-16 16:17:21 -0300

    see

--- damage_transfer.py
+++ damage_transfer.py
@@ -285,7 +285,7 @@ def main(year_init=0, year_end=N_YEARS, plot=False):
     else:
         p_failures_total = np.load(p_failure_arr, allow_pickle=True)['arr_0']
 
-    print(p_failures_total)
+    print(type(p_failures_total))
 
     x = np.arange(1980, 1980+len(p_failures_total["miner"]))
     fig, (tax, bax) = plt.subplots(2, 1, sharex=True)
