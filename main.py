663b63d3f9a8c3b67e7ecc92b8813ce6bb6675b9
hanzy1110 <hanzy1110@github.com>
2024-02-18 15:16:50 -0300

    more results

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from src.freqModel import total_cycles_per_year
 # )
 BASE_PATH = pathlib.Path(__file__).parent
 DATA_DIR = BASE_PATH / "data"
-RESULTS_FOLDER = BASE_PATH / "RESULTS"
+RESULTS_FOLDER = BASE_PATH / "RESULTS_2"
 PLOT_DIR = BASE_PATH / "plots"
 
 LOAD_PATH = RESULTS_FOLDER / "LOADS"
